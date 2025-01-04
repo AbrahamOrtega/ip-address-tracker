f44caa7149ba8f01ae97eb60850066422aaa65a3
Abraham Ortega <AbrahamOrtegaM@outlook.com>
2025-01-03 20:16:00 -0600

    mobile version

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import { icon } from "leaflet";
 import { useEffect } from "react";
+import Image from "next/image";
 
 interface MapComponentProps {
   position: [number, number];
@@ -29,7 +31,20 @@ export default function MapComponent({ position }: MapComponentProps) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position} />
+      <Marker
+        position={position}
+        icon={icon({
+          iconUrl: "/images/icon-location.svg",
+          iconSize: [32, 40],
+        })}
+      >
+        <Image
+          src="/images/icon-location.svg"
+          alt="Marker Icon"
+          width={30}
+          height={40}
+        />
+      </Marker>
       <MapViewUpdater position={position} />
     </MapContainer>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -34,6 +34,7 @@ export default function Home() {
     timezone: "-05:00",
     isp: "SpaceX Starlink",
   });
+  const [background, setBackground] = useState("");
 
   const handleRequest = async () => {
     if (validIP) {
@@ -70,12 +71,27 @@ export default function Home() {
     setValidIP(ipDomainValidation(ip));
   }, [ip]);
 
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    setBackground(getBackgroundImage());
+    window.addEventListener("resize", () => {
+      setBackground(getBackgroundImage());
+    });
+  }, []);
+
+  const getBackgroundImage = () => {
+    if (window.innerWidth < 768) {
+      return "/images/pattern-bg-mobile.png";
+    }
+    return "/images/pattern-bg-desktop.png";
+  };
+
   return (
-    <div className="flex flex-col w-full min-h-screen items-center gap-y-10 relative">
+    <div className="flex flex-col w-full min-h-screen items-center gap-y-6 lg:gap-y-10 px-8 lg:px-0 relative">
       <div className="flex flex-col absolute z-10 top-0 left-0 w-full h-full">
         <div className="flex w-full h-[300px]">
           <Image
-            src="/images/pattern-bg-desktop.png"
+            src={background}
             alt="Pattern Background"
             width={10000}
             height={1000}
@@ -87,9 +103,9 @@ export default function Home() {
       </div>
 
       <h1 className="mt-8 text-[32px] font-[500] z-20">IP Address Tracker</h1>
-      <div className="flex z-20">
+      <div className="flex w-full justify-center z-20">
         <input
-          className={`w-[500px] h-[56px] text-[18px] rounded-l-xl p-4 outline-none text-black ${
+          className={`w-full lg:w-[500px] h-[56px] text-[18px] rounded-l-xl p-4 outline-none text-black ${
             validIP === false && "border-red-500 border-2"
           } ${validIP === true && "border-green-600 border-2"}`}
           type="text"
@@ -98,7 +114,7 @@ export default function Home() {
           placeholder="Search for any IP address or domain"
         />
         <button
-          className="flex w-[48px] h-[56px] justify-center items-center bg-[#000] text-white rounded-r-xl"
+          className="flex w-[48px] h-[56px] justify-center items-center bg-black hover:bg-veryDarkGray text-white rounded-r-xl"
           onClick={handleRequest}
         >
           <Image
@@ -110,31 +126,31 @@ export default function Home() {
         </button>
       </div>
 
-      <div className="flex w-[80%] z-20 py-4 bg-white rounded-xl divide-x divide-solid">
-        <div className="flex flex-col w-1/4 py-6 px-6 justify-center gap-y-2">
+      <div className="flex flex-col items-center w-full lg:flex-row lg:w-[80%] z-20 py-8 gap-y-4 bg-white rounded-xl divide-y lg:divide-y-0 lg:divide-x divide-solid">
+        <div className="flex flex-col lg:w-1/4 px-6 justify-center items-center lg:items-start gap-y-2">
           <h2 className="text-[14px] font-[500] text-darkGray">IP ADDRESS</h2>
-          <p className="text-[32px] font-[500] text-veryDarkGray break-all">
+          <p className="text-[16px] md:text-[24px] lg:text-[28px] font-[500] text-veryDarkGray break-all">
             {locationData.ip}
           </p>
         </div>
 
-        <div className="flex flex-col w-1/4 py-6 px-6 justify-center gap-y-2">
+        <div className="flex flex-col lg:w-1/4 px-6 justify-center items-center lg:items-start gap-y-2">
           <h2 className="text-[14px] font-[500] text-darkGray">LOCATION </h2>
-          <p className="text-[32px] font-[500] text-veryDarkGray">
+          <p className="text-[16px] md:text-[24px] lg:text-[28px] font-[500] text-veryDarkGray">
             {locationData.location.city}, {locationData.location.region}
           </p>
         </div>
 
-        <div className="flex flex-col w-1/4 py-6 px-6 justify-center gap-y-2">
+        <div className="flex flex-col lg:w-1/4 px-6 justify-center items-center lg:items-start gap-y-2">
           <h2 className="text-[14px] font-[500] text-darkGray">TIME ZONE</h2>
-          <p className="text-[32px] font-[500] text-veryDarkGray">
+          <p className="text-[16px] md:text-[24px] lg:text-[28px] font-[500] text-veryDarkGray">
             UTC {locationData.timezone}
           </p>
         </div>
 
-        <div className="flex flex-col w-1/4 py-6 px-6 justify-center gap-y-2">
+        <div className="flex flex-col lg:w-1/4 px-6 justify-center items-center lg:items-start gap-y-2">
           <h2 className="text-[14px] font-[500] text-darkGray">ISP</h2>
-          <p className="text-[32px] font-[500] text-veryDarkGray">
+          <p className="text-[16px] md:text-[24px] lg:text-[28px] font-[500] text-veryDarkGray">
             {locationData.isp}
           </p>
         </div>
